44c3778ddbd90a8f94239b35ad2c2b8fd294d986
Luis Esteban Chacón <luis.chaconchaves@ucr.ac.cr>
2025-03-18 04:51:12 -0600

    Creación del proyecto y solución del laboratorio 1

---
+++ Laboratorio1_c22039/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Laboratorio1_c22039.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
