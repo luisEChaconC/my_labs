c08e74801963f03b888eef79c1fc401603b7f373
Luis Esteban Chacón <luis.chaconchaves@ucr.ac.cr>
2025-04-02 15:50:01 -0600

    Se añadio el laboratorio 3, de Vue.js

---
+++ Laboratorio3/frontend-lab/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <ListaPaises />
+</template>
+
+<script>
+import ListaPaises from "./components/ListaPaises.vue";
+
+export default {
+  name: 'App',
+  components: {
+    ListaPaises,
+  },
+};
+
+</script>
+
+<style>
+
+#app {
+}
+</style>
